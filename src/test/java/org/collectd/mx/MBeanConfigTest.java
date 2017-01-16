8aa2459fca7138d44369650302981634887338dd
christian.senet <christian.senet@hardis.fr>
2017-01-16 13:42:03 +0100

    update Test

--- src/test/java/org/collectd/mx/MBeanConfigTest.java
+++ src/test/java/org/collectd/mx/MBeanConfigTest.java
@@ -28,7 +28,7 @@ public class MBeanConfigTest extends TestCase {
         MBeanConfig config = new MBeanConfig();
         MBeanCollector collector = config.add("javalang");
         assertTrue(collector != null);
-        assertTrue(collector.getQueries().size() == 4);
+        assertTrue(collector.getQueries().size() == 7);
     }
 
     public void testJavaLangOS() throws Exception {
